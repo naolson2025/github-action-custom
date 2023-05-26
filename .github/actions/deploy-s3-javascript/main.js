e69feaf4a493a572814a8a32a08356a19d8574f2
Nick Olson <naolson.1819@gmail.com>
2023-05-26 09:11:04 -0500

    building out custom js action that pushes to s3 bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1. get some input values
+  // the action.yaml file has a list of inputs that this action expects
+  const bucket = core.getInput('bucket', { required: true })
+  const bucketRegion = core.getInput('bucket-region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
+
+  // 2. upload files to s3
+  // the aws cli is already installed in the github actions ubuntu-latest Runner environment
+  exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`)
+
   core.notice('hello world')
 }
 
